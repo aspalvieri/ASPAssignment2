639489fbad538fad1a41e468c650d317d959bdde
Craig <ayanamirei629@gmail.com>
2019-03-30 14:45:31 -0400

    seed
    seeds added

--- ASPAssignment2/Models/DatabaseContextInitializer.cs
+++ ASPAssignment2/Models/DatabaseContextInitializer.cs
@@ -10,7 +10,36 @@ namespace ASPAssignment2.Models
     {
         protected override void Seed(DatabaseContext context)
         {
+            Genre comedy = new Genre { Name = "Comedy", Description = "deep breath in your tough life" };
+            Genre fps = new Genre { Name = "FPS", Description = "Men's romance" };
+            Genre moba = new Genre { Name = "Moba", Description = "Uninstall button onclick" };
+            context.Genres.Add(comedy);
+            context.Genres.Add(fps);
+            context.Genres.Add(moba);
+            List<Reviews> csgoReviews = new List<Reviews>();
+            VideoGame csgo = new VideoGame
+            {
+                Name = "CSGO",
+                Price = 20,
+                Developer = "UK",
+                Publisher = "steam",
+                Description = "FPS Game",
+                Genre = fps,
+                Reviews = new List<Reviews>()
+            };
+            context.VideoGames.Add(csgo);
+            Reviews review = new Reviews
+            {
+                Name = "Craig",
+                Subject = "csgo",
+                Review = "I can't play",
+                Stars = 4,
+                VideoGame = csgo
+            };
+            
+
             base.Seed(context);
+
         }
     }
 }
