c37da6f8fe610739cb4488abdefb7f26d6f4b787
Craig <ayanamirei629@gmail.com>
2019-03-31 16:35:22 -0400

    Add simple jquery
    add some js

---
+++ ASPAssignment2/Scripts/app/gamescripts.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $("#titleword").mouseover(function () {
+        $(this).animate({ height: '+=25', width: '+=25' })
+            .animate({ height: '-=25', width: '-=25' });
+    });
+});
